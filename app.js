603ca4767882ea5e0646ba436c4a3afaa497621a
Minu Han <mwj1205@naver.com>
2024-09-23 20:36:34 +0900

    Feat: 전체 선수 목록, 플레이어가 소유한 선수 목록, 선수 상세 조회 API 추가
    - 게임에 존재하는 전체 선수 목록 조회
    - 모든 선수들의 기본 정보와 스텟을 반환
    - 플레이어가 소유한 선수의 목록 조회
    - 사용자 본인이 소유한 선수의 목록을 확인한다.
    - 다른 사용자의 선수 목록은 확인 불가
    - 선수 정보 상세 조회
    - 유저가 소유한 선수의 상세 정보를 확인
    - 인증 없이 누구나 확인 가능

--- app.js
+++ app.js
@@ -4,15 +4,23 @@ import userRouter from './routes/user.router.js';
 import playRouter from './routes/play.router.js';
 import rankingRouter from './routes/userRanking.router.js';
 import inquiryRouter from './routes/inquiry.router.js';
-import ErrorHandlerMiddleware from './middlewares/error-handler.middleware.js';
+import playerRouter from './routes/player.router.js';
 import SupportRouter from './routes/support.router.js';
+import ErrorHandlerMiddleware from './middlewares/error-handler.middleware.js';
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
 app.use(LogMiddleware);
-app.use('/api', [userRouter, playRouter, inquiryRouter, rankingRouter, SupportRouter]);
+app.use('/api', [
+  userRouter,
+  playRouter,
+  inquiryRouter,
+  rankingRouter,
+  playerRouter,
+  SupportRouter,
+]);
 
 app.use(ErrorHandlerMiddleware);
 
