5e5af32c7b83aa4f4d9b21be14a90ba50155d4d3
SoccermasterCR7 <jungjiwong06@gmail.com>
2024-09-23 14:25:02 +0900

    서포터 라우터 추가

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ import playRouter from './routes/play.router.js';
 import rankingRouter from './routes/userRanking.router.js';
 import inquiryRouter from './routes/inquiry.router.js';
 import ErrorHandlerMiddleware from './middlewares/error-handler.middleware.js';
-
+import SupportRouter from './routes/support.router.js';
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
 app.use(LogMiddleware);
-app.use('/api', [userRouter, playRouter, inquiryRouter, rankingRouter]);
+app.use('/api', [userRouter, playRouter, inquiryRouter, rankingRouter, SupportRouter]);
 
 app.use(ErrorHandlerMiddleware);
 
