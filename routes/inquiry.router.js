6d41d0fd1046c6b4534dd307a3645008a21f973b
Minu Han <mwj1205@naver.com>
2024-09-23 12:43:49 +0900

    Feat: 스쿼드 꾸리기 API 추가
    - 유저의 squad를 구성하는 API 추가
    - 반드시 3명의 선수로 이루어져야 함
    - 한 스쿼드에 중복된 선수가 존재해서는 안됨
    - 유저는 자신이 소유한 선수들로만 스쿼드를 구성해야 함
    - 스쿼드 생성에 성공했다면 스쿼드의 정보를 반환

--- routes/inquiry.router.js
+++ routes/inquiry.router.js
@@ -1,7 +1,19 @@
 import express from 'express';
 import { prisma } from '../utils/prisma/prismaClient.js';
+import authMiddleware from '../middlewares/auth.middleware.js';
+import Joi from 'joi';
 
 const router = express.Router();
+
+const teamComposeSchema = Joi.object({
+  player1Id: Joi.number().integer().required(),
+  player2Id: Joi.number().integer().required(),
+  player3Id: Joi.number().integer().required(),
+}).messages({
+  'any.required': '{{#label}}는 필수 입력 항목입니다.',
+  'number.base': '{{#label}}는 숫자여야 합니다.',
+  'number.integer': '{{#label}}는 정수여야 합니다.',
+});
 
 // 스쿼드 조회 함수
 async function getUserSquad(userId) {
@@ -54,6 +66,70 @@ router.get('/teams/:userId', async (req, res) => {
   }
 
   return res.json({ players: squadPlayers });
+});
+
+router.post('/teams', authMiddleware, async (req, res, next) => {
+  try {
+    const user = req.user;
+    const { player1Id, player2Id, player3Id } = await teamComposeSchema.validateAsync(req.body);
+
+    const playerIds = [player1Id, player2Id, player3Id];
+    if (new Set(playerIds).size !== playerIds.length) {
+      return res.status(400).json({ message: '중복된 선수를 선택할 수 없습니다.' });
+    }
+
+    const userCards = await prisma.userCard.findMany({
+      where: {
+        AND: [{ userid: user.id }, { id: { in: playerIds } }],
+      },
+      select: {
+        id: true,
+        playername: true,
+      },
+    });
+
+    // userCards에 존재하지 않는 playerIds를 찾아서 반환
+    const foundCardIds = userCards.map((card) => card.id);
+    const missingPlayerIds = playerIds.filter((id) => !foundCardIds.includes(id));
+
+    if (missingPlayerIds.length > 0) {
+      return res.status(404).json({
+        message: '선택된 선수가 존재하지 않습니다.',
+        missingPlayers: missingPlayerIds.join(', '),
+      });
+    }
+
+    // Squad가 존재했다면 update, 존재하지 않았다면 create
+    const updatedSquad = await prisma.squad.upsert({
+      where: { userid: user.id },
+      update: {
+        player1Id,
+        player2Id,
+        player3Id,
+      },
+      create: {
+        userid: user.id,
+        player1Id,
+        player2Id,
+        player3Id,
+      },
+    });
+
+    // 응답 데이터 생성
+    const response = {
+      message: '팀 구성이 완료되었습니다.',
+      squad: {
+        userid: updatedSquad.userid, // upsert된 데이터에서 가져오기
+        player1Id: updatedSquad.player1Id,
+        player2Id: updatedSquad.player2Id,
+        player3Id: updatedSquad.player3Id,
+      },
+    };
+
+    return res.status(200).json(response);
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default router;
